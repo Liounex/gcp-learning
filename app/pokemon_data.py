ad9bb57aa38bc316606be985bb9588750a8a8c3d
Jhimmy <pbbanger32@gmail.com>
2023-12-29 20:10:16 -0500

    nts

---
+++ app/pokemon_data.py
@@ -0,0 +1,5 @@
+pokemon_data = {
+    "bulbasaur": {"name": "Bulbasaur", "type": "Grass/Poison"},
+    "charmander": {"name": "Charmander", "type": "Fire"},
+    "squirtle": {"name": "Squirtle", "type": "Water"}
+}
